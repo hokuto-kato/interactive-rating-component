a9ac0548456242b31e1dd296b1fc4120685b2382
hokuto-kato <hokuto15@gmail.com>
2022-12-07 08:11:43 +0900

    [update #4]update color settings

---
+++ src/style/variable.jsx
@@ -0,0 +1,17 @@
+export const colors = {
+	ebonyClay: '#26283B',
+	mirage: '#161928',
+	veryDarkblue: '#131518',
+	darkBlue: '#262e38',
+	orange: '#fc7614',
+	pureWhite: '#ffffff',
+	lightGrey: '#969fad'
+}
+
+export const size = {
+	mobile: 412
+}
+
+export const device = {
+	mobile: `(min-width: ${size.mobile}px)`
+}
